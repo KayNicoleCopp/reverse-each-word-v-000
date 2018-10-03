7aa6922ef6472282b196cde274f662605b2d0e9f
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-03 02:03:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word(sentence)
   new_array = sentence.split(" ")
    new_array.collect do |words|
     new_sentence = words.reverse
-    another_new_sentence = new_sentence.join
+    another_new_sentence = new_sentence.join(" ")
   end
 end
