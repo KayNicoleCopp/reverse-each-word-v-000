a3b47167fd5eb9da38cb95e1d4a264b0e9be1362
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-03 01:58:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  binding.pry
-  sentence.collect do |words|
+  new_array = sentence.split(" ")
+   new_array.collect do |words|
     new_sentence = words.reverse
     new_sentence
   end
